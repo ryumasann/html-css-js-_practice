06abf046090bd7bde1d6e2c8b0d39c639505e99c
ryumasann <im20140319@gmail.com>
2024-03-16 19:49:31 +0900

    some, map, filter

--- js/hasodd.js
+++ js/hasodd.js
@@ -1,11 +1,16 @@
-function hasOdd(numbers) {
-  for (const num of numbers) {
-      if (num % 2 !== 0) {
-          return true; // 奇数が見つかった場合はtrueを返す
-      }
-  }
-  return false; // 配列を調べても奇数が見つからなかった場合はfalseを返す
-}
+// function hasOdd(numbers) {
+//   for (const num of numbers) {
+//       if (num % 2 !== 0) {
+//           return true; // 奇数が見つかった場合はtrueを返す
+//       }
+//   }
+//   return false; // 配列を調べても奇数が見つからなかった場合はfalseを返す
+// }
 
-// 関数を呼び出して奇数が含まれるかどうかを確認する
-console.log(hasOdd([1, 2, 3, 4, 5])); // true
+// // 関数を呼び出して奇数が含まれるかどうかを確認する
+// console.log(hasOdd([1, 2, 3, 4, 5])); // true
+
+const hasOdd = array => array.some(num => num % 2 !== 0);
+
+// 関数を使って奇数が含まれるかどうかを確認する
+console.log(hasOdd([1, 2, 3, 4, 5]));
