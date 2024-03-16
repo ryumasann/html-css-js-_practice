5cf531e41a6c5e48ae2dd062bb1d778d467c76aa
ryumasann <im20140319@gmail.com>
2024-03-16 18:59:43 +0900

    var

---
+++ js/var.js
@@ -0,0 +1,12 @@
+// 値の再代入できない変数を定義し、それぞれの値を代入
+const num_1 = 5;
+const num_2 = 3;
+
+// 値の再代入が可能な変数を定義し、num_1 と num_2 を足した値を代入
+let sum = num_1 + num_2;
+
+// 値の再代入が可能な変数を定義し、num_1 と num_2 を引いた値を代入
+let difference = num_1 - num_2;
+
+// sum と difference を足した値を出力
+console.log(sum + difference);
